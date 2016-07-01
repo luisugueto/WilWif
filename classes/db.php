9b898e8ad9a4e8545c27eb4a39d831716df7e68a
Luigi <blink242@outlook.com>
2016-06-30 19:54:53 -0430

    User

--- classes/db.php
+++ classes/db.php
@@ -19,6 +19,21 @@ class DB{
 		 }
 	}
 
+	public function loginBackOffice($username, $password){
+		$sql = 'SELECT username, password, id, rol_id FROM user WHERE username = "'.$username.'"';
+		$query = mysql_query($sql) or die('error at try to access data' . mysql_error());
+		$row = mysql_fetch_assoc($query);
+		echo $row['password'];
+		echo "user pass:".$password;
+		if(($username == $row['username']) && ($password == $row['password']) && ($row['rol_id'] == 1 || $row['rol_id'] == 2)){
+		 	    $_SESSION['loggedin'] = true;
+		 	    $_SESSION['username'] = $row['username'];
+		 	    $_SESSION['id'] = $row['id'];
+		 	    $_SESSION['rol_id'] = $row['rol_id'];
+		 	    return true;
+		 }
+	}
+
 	public function logout(){
 		session_destroy();
 	}
