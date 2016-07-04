b50eb98c498c2f0d9c7bab8b1541e552fe8e2e0e
zuaru <zuaru@zuaru.com>
2016-07-04 13:56:35 -0400

    fix path names

--- public_html/index.php
+++ public_html/index.php
@@ -15,35 +15,22 @@ if($path_urls[1])
  $path = "";
 }
 switch ($path){
-	case 'newitem':
-	   include 'view/page-new-item.php';
-	break;
-	case 'item':
-	   include 'view/page_item.php';
-	break;
 	
-    case 'execution':
-	   include '../execution-controler.php';
-	break;
+	case 'item':
+	   include 'view/page-item.php';
+		break;
 	
 	case 'login':
 	  include 'view/page-login.php';
-	break;
+		break;
 	
 	case 'register':
-	 include 'view/page_register.php';
-	break;
+	 include 'view/page-register.php';
+		break;
 	
-	case 'nuevoItem':
-	 include 'view/page-newItem.php';
-	break;
-
-	case 'orderItem':
-	 include 'view/page-order-submit-item.php';
-	break;
-	case 'receiveItem':
-	 include 'view/page-order-submit-item.php';
-	break;
+	case 'account-lost':
+	 include 'view/page-account-lost.php';
+		break;
 
 	case 'account':
 		if($path_urls[1])
@@ -53,38 +40,78 @@ switch ($path){
 		 $secundary_path = "";
 		}
 		switch ($path){
-			case 'found':
-				include'view/page-user-found.php';
-				break;
-				
-			case 'lost':
-				include'view/page-user-lost-articles.php';
+			case 'info':
+				include'view/page-user-information.php';
 				break;
 			
-			case 'send':
-				include'view/page-user-send-articles.php';
+			case 'found-item':
+				include'view/page-user-found-item.php';
+				break;
+				
+			case 'found-items':
+				include'view/page-user-found-items.php';
+				break;
+				
+			case 'lost-item':
+				include'view/page-user-lost-item.php';
+				break;
+				
+			case 'lost-items':
+				include'view/page-user-lost-items.php';
+				break;
+			
+			case 'shipment':
+				include'view/page-user-shipment.php';
+				break;.
+			
+			case 'shipments':
+				include'view/page-user-shipments.php';
+				break;
+				
+			case 'order':
+				include'view/page-user-order.php';
+				break;.
+			
+			case 'orders':
+				include'view/page-user-orders.php';
+				break;
+			
+			case 'request':
+				include'view/page-user-request.php';
+				break;.
+			
+			case 'requests':
+				include'view/page-user-requests.php';
+				break;
+			
+			case 'notification':
+				include'view/page-user-notification.php';
+				break;.
+			
+			case 'notifications':
+				include'view/page-user-notifications.php';
 				break;
 				
 			default:
-				include 'view/page-user.php';
+				include 'view/page-account.php';
 				break;
 		}
 		
 	break;
-	case 'foundView':
-		include'view/page-user-found-view.php';
-	break;
+	
+	case 'execution':
+	   include '../execution-controler.php';
+		break;
+	
 	case 'logout':
-
-	$history = "INSERT INTO history (id_user, action, date) VALUES('".$_SESSION['id']."', 'It closed session.', NOW())";
-	$query_history = mysql_query($history) or die('error at try to access data' . mysql_error());
-
-	 session_destroy();
-	 header('Location: /');
-	break;
+		$history = "INSERT INTO history (id_user, action, date) VALUES('".$_SESSION['id']."', 'It closed session.', NOW())";
+		$query_history = mysql_query($history) or die('error at try to access data' . mysql_error());
+		session_destroy();
+		header('Location: /');
+		break;
 	
 	default:
-	 include 'view/page_home.php';
-	break;
+		include 'view/page_home.php';
+		break;
 }
 
