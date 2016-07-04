b2f7abca3b07f9f07535504ad7b22b93cb968be4
Luigi <blink242@outlook.com>
2016-07-04 16:12:34 -0430

    Account

--- admin/layout/header.php
+++ admin/layout/header.php
@@ -62,9 +62,9 @@
         </li>
         <?php } ?>
         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Objetos <span class="caret"></span></a>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Account<span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="/nuevoItem/">Nuevo Objeto</a></li>
+            <li><a href="/account/">Account</a></li>
             <li><a href="#">Another action</a></li>
             <li><a href="#">Something else here</a></li>
             <li role="separator" class="divider"></li>
