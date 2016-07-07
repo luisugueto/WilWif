b436e32226c9c8b07e5a412c706d92ae864af2ff
Luis Ugueto <blink242@outlook.com>
2016-07-06 19:38:13 -0430

    Edit Login - Register

--- public_html/view/page-account.php
+++ public_html/view/page-account.php
@@ -9,8 +9,8 @@ $title = 'Inicio';
 //include header template
 require('layout/header.php'); 
 ?>
-<div id="main-area">
-	<div id="main-content-bg">
+<!-- <div id="main-area">
+	 <div id="main-content-bg">
     	<div id="main-content" style="display:table;">
         	<div style="display:table-cell; vertical-align:middle;">
         	<a href="info"><div class="item img-shadow"><img style="margin-right:40px;" class="img-shadow" src="../image/icono_info_21x39.png" width="21px" height="39px" />Account info</div></a>
@@ -19,6 +19,12 @@ require('layout/header.php');
             <a href="shipments"><div class="item img-shadow"><img style="margin-right:40px;" class="img-shadow" src="../image/icono_enviado_23x38.png" width="21px" height="39px" />Send articles</div></a>
             </div>
         </div>
+    </div> 
+        
+</div> -->
+<div align="center">
+    <div class="contenedorLogin" style="padding-bottom: 20px; width: 800px ">
+        <input class="submit" style="top: 330px; margin-left: 30px;" type="submit" name="submit" value="" tabindex="6">
     </div>
 </div>
 <?php 
