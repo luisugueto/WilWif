7b958a521a0ea043f452b43a9416ba32a5fbeb5b
zuaru <zuaru@zuaru.com>
2016-07-06 22:00:21 -0400

    s

--- admin/view/page-chats.php
+++ admin/view/page-chats.php
@@ -5,7 +5,7 @@ require('layout/header.php');
 <div id="content">
 <div  style="height: 112px; background-image: url('/image/header2-1440-112.png'); background-repeat: no-repeat; background-size: 100% auto; width: 100%;">
 	<div style="background-image: url('/image/barra-chats-534-78.png'); background-repeat: no-repeat; width: 540px; height: 82px; display: inline-block; margin-left: -425px; margin-top: 15px;">
-		<h1 style="height: 38px; color: white; width: 220px; font-family: arial,rial;margin-left: 83px;">CHATS</h1>
+		<h2 style="height: 38px; color: white; width: 270px; font-family: arial,rial; margin-left: 83px; margin-top: 19px; padding-top: 6px; border-right-width: 0px; padding-right: 50px;">CHATS OPTIONS</h2>
 	</div>
 </div>
 <div id="content_containter" style="margin-top: 50px; margin-bottom: 50px; width: 1440px; display: inline-block;">
--- admin/view/page-notifications.php
+++ admin/view/page-notifications.php
@@ -5,7 +5,7 @@ require('layout/header.php');
 <div id="content">
 <div  style="height: 112px; background-image: url('/image/header2-1440-112.png'); background-repeat: no-repeat; background-size: 100% auto; width: 100%;">
 	<div style="background-image: url('/image/barra-notificaciones-534-78.png'); background-repeat: no-repeat; width: 540px; height: 82px; display: inline-block; margin-left: -425px; margin-top: 15px;">
-		<h1 style="height: 38px; color: white; width: 220px; font-family: arial,rial;margin-left: 83px;">NOTIFICATIONS</h1>
+		<h2 style="height: 38px; color: white; width: 220px; font-family: arial,rial; margin-left: 83px; padding-top: 10px;">NOTIFICATIONS</h2>
 	</div>
 </div>
 <div id="content_containter" style="margin-top: 50px; margin-bottom: 50px; width: 1440px; display: inline-block;">
