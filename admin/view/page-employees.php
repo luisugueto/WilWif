198b8cea98be0838328afc8481913a30211bd728
Luis Ugueto <blink242@outlook.com>
2016-07-11 15:38:23 -0430

    Employees

--- admin/view/page-employees.php
+++ admin/view/page-employees.php
@@ -1,114 +1,177 @@
-<?php
-require('layout/header.php');
+<?php 
+//include header template
 
-$query = "SELECT * FROM user WHERE rol_id = '2'";
+$searchValue = (isset($_GET['s']))?  $_GET['s'] : '';
+if($searchValue =='')
+$searchValue = (isset($_POST['s']))?  $_POST['s'] : '';
+
+$searchValue = ($searchValue == '' )? '':" and (username like '%".$searchValue."%' or email like '%".$searchValue."%' or name like '%".$searchValue."%' )";
+$query = "SELECT * FROM user WHERE rol_id in (select id from rol where code !='001' and code !='010')".$searchValue;
+ 
 $sql = mysql_query($query);
 $sql_assoc = mysql_fetch_assoc($sql);
-$sql_row = mysql_num_rows($sql);
+$total = mysql_num_rows($sql);
+$total = ($total < 1)?1: $total;
+$nrows = 10;
+$totalpages = ceil($total/$nrows);
+$page = isset($_POST['page'])? $_POST['page']:1;
+######### PAGINACION ###############
 
 
-######### PAGINACIONN ###############
-$nregistros = 4;
-$nfilas = mysql_num_rows($sql);
-$numpags = $nfilas / $nregistros;
-if (isset($_POST['pagina']))	$npagina = $_POST['pagina']; else $npagina = 1;
+$query .= " LIMIT ".((($page*$nrows)-($nrows-1))-1).", ".$nrows;
+$sql = mysql_query($query);
+$records = mysql_num_rows($sql);
 
-$query .= " LIMIT ".((($npagina*$nregistros)-($nregistros-1))-1).", ".$nregistros;
-$resultado = mysql_query($query);
-$rows = mysql_num_rows($resultado);
+
 
 ############################################
 
-?>
 
+
+require('layout/header.php'); 
+?>
 <div id="content">
-<div  style="height: 112px; background-image: url('/image/header2-1440-112.png'); background-repeat: no-repeat; background-size: 100% auto; width: 100%;">
-	<div style="width: 1440px; display: inline-block; padding-right: 81px; padding-left: 221px; text-align: left;">
-		<div style="background-image: url('/image/barra-usuarios-534-78.png'); background-repeat: no-repeat; height: 82px; display: inline-block; margin-left: 0px; margin-top: 15px; width: 540px; padding-left: 90px;">
-			<h1 style="height: 38px; color: white; width: 270px; font-family: arial,rial;">EMPLOYEES</h1>
+<div class="header_div_1">
+	<div class="header_div_2">
+		<div id="menu_button">
+		
 		</div>
-		<form method="get" action="/" style="float: right; background-image: url('/image/barra-generica-478-47.png'); border-width: 0px; margin-top: 30px; background-color: transparent; background-repeat: no-repeat; background-size: 100% 100%; padding-top: 1px; padding-right: 66px; padding-left: 0px; width: 386px; height: 51px;">
-			<p style="float: left; width: 82px; padding-left: 17px; color: white; font-size: 20px; margin-top: 13px;">Search</p>
-			<input type="text" value="<?php if(isset($_GET['s'])){ echo $_GET['s']; }?>" name="s" id="search_value" style="border-width: 0px; margin-top: 0px; background-color: transparent; background-repeat: no-repeat; background-size: 100% 100%; padding-top: 1px; padding-right: 0px; padding-left: 0px; height: 51px; float: left; width: 238px;">
+		<div class="header_div_3 header_div_home">
+			<h2 class="header_title_1">EMPLOYEES</h2>
+		</div>
+		<form class="form_search" method="get" action="" >
+			<p >Search</p>
+			<input type="text" value="<?php if(isset($_GET['s'])){ echo $_GET['s']; }?>" name="s" id="search_value">
 		</form>
 	</div>
 </div>
+<div>
+	<div id="menu" class="menu_close">
 	
-<div id="content_containter" style="margin-top: 40px; margin-left: -120px; margin-bottom: 50px; width: 1440px; display: inline-block;">
-
-		
-		<div style="border-radius: 50px; margin-left: 0px; margin-top: 20px;">
-			<table style="border-color: white; border-radius: 50px; width: 1100px; display: inline-block; background-color: rgba(096,111,140,0.3); " border="4px">
-				<thead style="border: 5px;" >
-					<tr>
-						<td width="400px" style="border-bottom: 0px solid; border-top: 0px; border-left: 0px; border-right: 0px solid; border-color: white;"><p style="color: white">User Name</p></td>
-						<td width="300px" style="border: 5px solid; border-bottom: 0px solid; border-top: 0px; border-left: 5px solid; border-right: 5px solid; border-color: white;"><p style="color: white">Email</p></td>
-						<td width="300px" style="border: 5px solid; border-bottom: 0px solid; border-top: 0px; border-left: 0px; border-right: 5px solid; border-color: white;"><p style="color: white">Status</p></td>
-						<td width="300px" style="border-bottom: 0px solid; border-top: 0px; border-left: 0px; border-right: 0px solid; border-color: white"><p style="color: white">Options</p></td>
-					</tr>
-				</thead>
-				<tbody>
-				<?php
-						
-						if($sql_row == 0)
-						{
-							echo "<tr>
-									<td colspan='6' style='border-bottom: 0px solid; border-top: 5px solid; border-left: 0px; border-right: 0px solid; border-color: white;'><p style='color: white'>No have.</p></td>
-								</tr>";
-						}
-						while($sql_assoc = mysql_fetch_assoc($resultado)){
-					?>
-					<tr>
-						<td width="300px" style="border: 5px solid; border-bottom: 0px solid; border-top: 5px solid; border-left: 0px solid; border-right: 0px solid; border-color: white;"><p style="color: white"><?php echo $sql_assoc['username']; ?></p></td>
-						<td width="300px" style="border: 5px solid; border-bottom: 0px solid; border-top: 5px solid; border-left: 5px solid; border-right: 5px solid; border-color: white;"><p style="color: white"><?php echo $sql_assoc['email']; ?></p></td>
-						<td width="300px" style="border-bottom: 0px solid; border-top: 5px solid; border-left: 5px; border-right: 5px solid; border-color: white"><p style="color: white"><?php echo $sql_assoc['status']; ?></p></td>
-						<td width="300px" style="border: 5px solid; border-bottom: 0px solid; border-top: 5px solid; border-left: 0px; border-right: 0px solid; border-color: white;"><p style="color: white">
-						<form action="/employees/viewemployee" method="post">
-							<input type="hidden" value="<?php echo $sql_assoc['id']; ?>" name="id" id="id">
-							<input class="btn btn-primary" type="submit" id="view" name="view" value="" style="background:url('/image/ver-56-56-02.png'); width: 60px; height: 60px; border: 0px">
-						</form>
-
-
-						</p></td>
-					</tr>
-					<?php } ?>
-			
-				</tbody>
-			</table>
-
-
-		<div style="width: 890px; display: inline-block; padding-top: 10px; padding-bottom: 10px;">
-		
-			<div style="clear: both; content: ''; display: table; float: left; margin-left: 400px">
-				<div style="float: left; margin-right: 20px;">
-					<?php echo "<a style='text-decoration: none;' href='/'>";?>
-						<img width="50" height="50" src="/image/boton-volver-57-57.png" style="cursor: pointer;">
-						<p style="color: white; width: 62px; margin-top: 0px; margin-bottom: 0px;">Return</p>
-					</a>
-				</div>
-				<div style="float: left; margin-right: 20px;">
-					<?php echo "<a style='text-decoration: none;' href='/employees/employee'>";?>
-						<img width="50" height="50" src="/image/boton-crear-40-40.png" style="cursor: pointer;">
-						<p style="color: white; width: 62px; margin-top: 0px; margin-bottom: 0px;">Add</p>
-					</a>
-				</div>
-			</div>
-		
-		</div>
-
-			<div style="postion:relative; float: right; margin-top: -80px; margin-right: 0px">
-					<form action="" method="post">
-						<input type="hidden" value="<?php echo $npagina ?>" id="npag">
-					<?php
-						for ($i=1; $i <= $numpags; $i++) { 
-							echo '<input type="submit" value="'.$i.'" name="pagina" id="pagina">';
-						}
-					?>
-					</form>
-			</div>
-		</div>
+	</div>
 </div>
+<div id="content_containter">
+	
+	<div class="content_result_div">
+		<div class="content_grid_result">
+			<div>
+				<div class="header_container">
+				<div class="header_container_result">
+					<div class="header_column_result header_column_1_5 column_cel_1_3">
+						USERNAME
+					</div>
+					<div class="header_column_result header_column_1_5 column_cel_1_3">
+						E-MAIL
+					</div>
+					<div class="header_column_result header_column_1_5 column_cel_no_display">
+						NAME
+					</div>
+					<div class="header_column_result header_column_1_5 column_cel_no_display">
+						STATUS
+					</div>
+					<div class="header_column_result header_column_1_5 column_cel_1_3">
+						OPTIONS
+					</div>
+				</div>
+				</div>
+				<div class="result_container">
+					<?php 
+					
+						while($row = mysql_fetch_assoc($sql))
+						{
+						?>
+						<div class="row_container_result">
+							<div class="row_column_result header_column_1_5 column_cel_1_3">
+								<?php echo $row['username']; ?>
+							</div>
+							<div class="row_column_result header_column_1_5 column_cel_1_3">
+								<?php echo $row['email']; ?>
+							</div>
+							<div class="row_column_result header_column_1_5 column_cel_no_display">
+								<?php echo $row['name']; ?>	
+							</div>
+							<div class="row_column_result header_column_1_5 column_cel_no_display">
+								<?php echo $row['status']; ?>
+							</div>
+							<div class="row_column_result header_column_1_5  column_cel_1_3">
+								<form action="/employees/viewemployee" method="post">
+								<input type="hidden" value="<?php echo $sql_assoc['id']; ?>" name="id" id="id">
+								<input class="btn btn-primary" type="submit" id="view" name="view" value="" style="background:url('/image/ver-56-56-02.png'); width: 60px; height: 60px; border: 0px">
+								</form>
+							</div>
+						</div>
+						<?php
+					}
+					?>
+				</div>
+				<div class="pages_container">
+					<div class="pages_container_index" style="display: inline-flex;">
+					<?php 
+						$maxi = ($page+2 <= $totalpages )? $page+2: (($page+1 <= $totalpages )? $page+1: $totalpages);
+						$mini = ($page-2 >= 1 )? $page-2: (($page-1 >= 1 )? $page-1: 1);
+						for($i = $mini ; $i<= $maxi;$i++)
+						{
+							if($i ==$page-2 && $i != 1)
+							{
+								?>
+									<form action="" method="post">
+										<input type="hidden" name="page" value=1>
+										<input type="hidden" name="s" value="<?php if(isset($_POST['s'])){echo $_POST['s'];}?>">
+										<input submit class="page_index" value ="1.">
+									</form>
+								<?php
+							}
+							if($i == $page)
+							{
+								?>
+									<input type="submit" class="page_index current_page" value ="<?php echo $i;?>">
+								<?php
+							}else{
+								?>
+									<form action="" method="post">
+										<input type="hidden" name="page" value="<?php echo $i;?>">
+										<input type="hidden" name="s" value="<?php if(isset($_POST['s'])){echo $_POST['s'];}?>">
+										<input type="submit" class="page_index" value ="<?php echo $i;?>">
+									</form>
+								<?php
+							
+							}
+			
+							if($i == $page+2 && $i != $totalpages)
+							{
+								?>
+									<form action="" method="post">
+										<input type="hidden" name="page" value="<?php echo $totalpages;?>">
+										<input type="hidden" name="s" value="<?php if(isset($_POST['s'])){echo $_POST['s'];}?>">
+										<input type="submit" class="page_index" value =".<?php echo $totalpages;?>">
+									</form>
+								<?php
+							}
+						}
+					?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</div>
+<script>
+	$("#menu_button").click(function() {
+		if($("#menu").hasClass( "menu_open" ))
+		{
+			$("#menu").removeClass( "menu_open" );
+			$("#menu").addClass( "menu_close" );
+		}else{
+			$("#menu").removeClass( "menu_close" );
+			$("#menu").addClass( "menu_open" );
+		}
+	});
+	
+</script>
+<style>
 
+</style>
 <?php 
 //include header template
 require('layout/footer.php');
